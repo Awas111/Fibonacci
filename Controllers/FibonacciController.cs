e7a0a54c067f3baf907e215e0436a5cb209a3c98
Ayesha Waseem <waseemayesha01@gmail.com>
2021-08-09 08:34:40 +0100

    first commit

---
+++ Controllers/FibonacciController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Fibonacci.Models;
+
+namespace Fibonacci.Controllers
+{
+    public class FibonacciController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FibonacciModel model)
+        {
+            if (model.limit < 2 || model.limit >= 4000000)// first check limit
+            {
+                ViewBag.ErrorMessage = "Enter valid limit between 2 - 4000000";
+            }
+            else
+            {
+                long firstEN = 0;
+                long secondEN = 2;
+                long Sum = firstEN + secondEN;
+                while (secondEN < model.limit)
+                {
+                    long thirdEN = 4 * secondEN + firstEN;
+                    if (thirdEN > model.limit)
+                        break;
+                    firstEN = secondEN;
+                    secondEN = thirdEN;
+                    Sum += secondEN;
+                }
+                model.Sum = Sum;
+            }
+                return View(model);
+            
+        }
+    }
+}
---
+++ Models/FibonacciModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Fibonacci.Models
+{
+    public class FibonacciModel
+    {
+        public long limit { get; set; }
+      
+        public long Sum { get; set; }
+    }
+}
